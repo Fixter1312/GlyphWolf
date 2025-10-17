95e559215c5eeaaca8d325d363e36022748a7852
Fixter1312 <thefixterpl@gmail.com>
2025-10-17 23:44:59 +0200

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,18 @@
+pluginManagement {
+    repositories {
+        google()            // <-- potrzebne, tu jest Android Gradle Plugin
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
 rootProject.name = "GlyphWolf"
 include(":app")
